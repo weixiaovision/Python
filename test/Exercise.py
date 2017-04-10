5693f93b72d92d263b8762f7a5d69c27bb2a652b
runehero <185858747@qq.com>
2017-04-10 10:37:54 +0800

    modified Exercise.py

--- test/Exercise.py
+++ test/Exercise.py
@@ -2,7 +2,11 @@
 # -*- coding:utf-8 -*-
 
 
-print(range(5))
-
-for i in range(5):
-	print
+list1 = [1]
+list2 = [1, 1]
+list3 = [1, 2 ,1]
+list4 = []
+list4[0] = 1
+list4[1] = list3[0] + list3[1]
+list4[2] = list3[1] + list3[2]
+list4[3] = 1
